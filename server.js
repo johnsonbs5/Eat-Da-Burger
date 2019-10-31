8faf7cb97c2566af8bfb0e56365b69f2749ec410
Bishop Brooks <jbohnson34@gmail.com>
2019-10-31 17:48:42 -0500

    fixed issues with mySQL

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+
 var port = process.env.PORT || 8080;
 
 var app = express();
@@ -21,5 +22,7 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-app.listen(port);
+app.listen(port, function() {
+    console.log("App listening on PORT " + port);
+  });
 
